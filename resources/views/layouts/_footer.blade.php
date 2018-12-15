2b30c39ab251d7acf4a7a40026d70597c61b5cc6
Flourishing <huangxiangrong827@163.com>
2018-12-15 20:50:20 +0000

    基础布局

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="http://weibo.com/u/3094689665?is_hot=1" target="_blank">黄向荣</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="mailto:huangxiangrong827@163.com">联系我们</a></p>
+    </div>
+</footer>
