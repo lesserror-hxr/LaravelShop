92b9b5f68be81b56951ef4c16657d45dfae6a474
Flourishing <huangxiangrong827@163.com>
2018-12-22 05:29:53 +0000

    购物车页面

--- routes/web.php
+++ routes/web.php
@@ -26,10 +26,11 @@ Route::middleware('auth')->group(function (){
         Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
         Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
         Route::post('cart', 'CartController@add')->name('cart.add');
+        Route::get('cart', 'CartController@index')->name('cart.index');
+        Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
         Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
     });
 });
-
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
