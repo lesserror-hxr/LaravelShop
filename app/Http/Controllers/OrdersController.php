18afef395a54470fa44b11babc217160dab4003f
Flourishing <huangxiangrong827@163.com>
2018-12-23 12:19:27 +0000

    用户界面 - 订单列表页

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -7,6 +7,7 @@ use App\Models\Order;
 use App\Jobs\CloseOrder;
 use App\Models\ProductSku;
 use App\Models\UserAddress;
+use Illuminate\Http\Request;
 use App\Http\Requests\OrderRequest;
 
 class OrdersController extends Controller
@@ -70,5 +71,17 @@ class OrdersController extends Controller
         $this->dispatch(new CloseOrder($order, config('app.order_ttl')));
 
         return $order;
+    }
+
+    public function index(Request $request)
+    {
+        $orders = Order::query()
+            // 使用 with 方法预加载，避免N + 1问题
+            ->with(['items.product', 'items.productSku'])
+            ->where('user_id', $request->user()->id)
+            ->orderBy('created_at', 'desc')
+            ->paginate();
+
+        return view('orders.index', ['orders' => $orders]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,7 @@ Route::middleware('auth')->group(function (){
         Route::get('cart', 'CartController@index')->name('cart.index');
         Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
         Route::post('orders', 'OrdersController@store')->name('orders.store');
+        Route::get('orders', 'OrdersController@index')->name('orders.index');
         Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
     });
 });
