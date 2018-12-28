6d583b3c868ccd86d8648ab6cea25257c56452c1
Flourishing <huangxiangrong827@163.com>
2018-12-28 13:59:49 +0000

    优化优惠券模块

--- app/Http/Controllers/CouponCodesController.php
+++ app/Http/Controllers/CouponCodesController.php
@@ -3,17 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\CouponCode;
+use Illuminate\Http\Request;
 use App\Exceptions\CouponCodeUnavailableException;
 
 class CouponCodesController extends Controller
 {
-    public function show($code)
+    public function show($code,Request $request)
     {
         if (!$record = CouponCode::where('code', $code)->first()) {
             throw new CouponCodeUnavailableException('优惠券不存在');
         }
 
-        $record->checkAvailable();
+        $record->checkAvailable($request->user());
 
         return $record;
     }
