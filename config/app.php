26ce9aebd8ade926051f0537e51ac8a067d223e0
Flourishing <huangxiangrong827@163.com>
2019-01-17 17:33:09 +0800

    fix bug

--- config/app.php
+++ config/app.php
@@ -232,8 +232,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-//        'Clockwork' => Clockwork\Support\Laravel\Facade::class,
-
     ],
 
 
