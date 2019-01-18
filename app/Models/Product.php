b0d72e0b5495013f04e9d633b0163c5f84ba29ae
Flourishing <huangxiangrong827@163.com>
2019-01-19 00:23:31 +0800

    初始化秒杀模型

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -13,9 +13,12 @@ class Product extends Model
 
     const TYPE_NORMAL = 'normal';
     const TYPE_CROWDFUNDING = 'crowdfunding';
+    const TYPE_SECKILL = 'seckill';
+
     public static $typeMap = [
         self::TYPE_NORMAL  => '普通商品',
         self::TYPE_CROWDFUNDING => '众筹商品',
+        self::TYPE_SECKILL => '秒杀商品',
     ];
 
     protected $fillable = [
@@ -111,5 +114,10 @@ class Product extends Model
         });
 
         return $arr;
+    }
+
+    public function seckill()
+    {
+        return $this->hasOne(SeckillProduct::class);
     }
 }
