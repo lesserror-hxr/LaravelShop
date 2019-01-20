42d4d4efc8cfe79098a499d0f5c1bc53575e927e
Flourishing <huangxiangrong827@163.com>
2019-01-21 02:46:38 +0800

    优化收货地址页面

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -64,6 +64,11 @@
                                         @endforeach
                                     </select>
                                 </div>
+
+                                <div class="col-sm-2 col-md-2">
+                                    <a href="{{route('user_addresses.create')}}" class="btn btn-danger">添加收货地址</a>
+                                </div>
+
                             </div>
                             <div class="form-group">
                                 <label class="control-label col-sm-3">备注</label>
