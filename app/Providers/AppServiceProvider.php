7e1687aee94671440309c576626fe515ba5eba00
Flourishing <huangxiangrong827@163.com>
2019-01-20 07:25:22 +0000

    减少秒杀接口 SQL 查询

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,10 +4,11 @@ namespace App\Providers;
 
 use Auth;
 use View;
-use Illuminate\Support\ServiceProvider;
-use Yansongda\Pay\Pay;
 use Monolog\Logger;
+use Yansongda\Pay\Pay;
 use Studio\Totem\Totem;
+use Illuminate\Support\Str;
+use Illuminate\Support\ServiceProvider;
 use Elasticsearch\ClientBuilder as ESClientBuilder;
 use App\Http\ViewComposers\CategoryTreeComposer;
 
@@ -25,6 +26,13 @@ class AppServiceProvider extends ServiceProvider
         // 当 Laravel 渲染 products.index 和 products.show 模板时，就会使用 CategoryTreeComposer 这个来注入类目树变量
         // 同时 Laravel 还支持通配符，例如 products.* 即代表当渲染 products 目录下的模板时都执行这个 ViewComposer
         View::composer(['products.index', 'products.show'],CategoryTreeComposer::class);
+
+        // 只在本地开发环境启用 SQL 日志
+        if (app()->environment('local')) {
+            \DB::listen(function ($query) {
+                \Log::info(Str::replaceArray('?', $query->bindings, $query->sql));
+            });
+        }
     }
 
     /**
--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -141,19 +141,19 @@ class OrderService
         return $order;
     }
 
-    //秒杀订单下单逻辑
-    public function seckill(User $user, UserAddress $address, ProductSku $sku)
+    // 将原本的 UserAddress 类型改成 array 类型
+    public function seckill(User $user, array $addressData, ProductSku $sku)
     {
-        $order = \DB::transaction(function () use ($user, $address, $sku) {
-            // 更新此地址的最后使用时间
-            $address->update(['last_used_at' => Carbon::now()]);
+        $order = \DB::transaction(function () use ($user, $addressData, $sku) {
+            // 将之前的更新收货地址的最后使用时间代码删除
             // 创建一个订单
             $order = new Order([
-                'address'      => [ // 将地址信息放入订单中
-                    'address'       => $address->full_address,
-                    'zip'           => $address->zip,
-                    'contact_name'  => $address->contact_name,
-                    'contact_phone' => $address->contact_phone,
+                'address'      => [ // address 字段直接从 $addressData 数组中读取
+
+                    'address'       => $addressData['province'].$addressData['city'].$addressData['district'].$addressData['address'],
+                    'zip'           => $addressData['zip'],
+                    'contact_name'  => $addressData['contact_name'],
+                    'contact_phone' => $addressData['contact_phone'],
                 ],
                 'remark'       => '',
                 'total_amount' => $sku->price,
