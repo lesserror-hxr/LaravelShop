b408dc58875eb0865dbeeb2f3f6cab8256394572
Flourishing <huangxiangrong827@163.com>
2019-01-21 03:14:16 +0800

    优化商品列表页面

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -109,9 +109,9 @@
                                         <div class="price" style="display: inline-block"><b>￥</b>{{ $product->price }}</div>
                                         @if($product->type == \App\Models\Product::TYPE_SECKILL)
                                             @if(!$product->seckill->is_before_start && !$product->seckill->is_after_end)
-                                                <div  style="display: inline-block;margin-top:10px;margin-right: 20px;float:right;border-radius:10%;border: 1px solid red;color:red;padding:0 8px;">秒杀中</div>
+                                                <div  style="display: inline-block;margin-top:10px;margin-right: 20px;float:right;border-radius:10%;border: 1px solid red;color:red;padding:0 8px;">秒杀中...</div>
                                             @else
-                                                <div  style="display: inline-block;margin-top:10px;margin-right: 20px;float:right;border-radius:10%;border: 1px solid black;color:black;padding:0 8px;">秒杀</div>
+                                                <div  style="display: inline-block;margin-top:10px;margin-right: 20px;float:right;border-radius:10%;border: 1px solid #EA638C;color:#EA638C;padding:0 8px;">秒杀</div>
                                             @endif
                                         @elseif($product->type == \App\Models\Product::TYPE_CROWDFUNDING)
                                             <div  style="display: inline-block;margin-top:10px;margin-right: 20px;float:right;border-radius:10%;border: 1px solid #2ab27b;color:#2ab27b;padding:0 8px;">拼团</div>
