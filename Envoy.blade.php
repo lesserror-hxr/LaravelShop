b72b8a185797863ed1b9d837d2f7a1d61beb0b81
Flourishing <huangxiangrong827@163.com>
2019-04-05 15:14:32 +0800

    add some view file

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,6 +1,7 @@
 @servers(['web' => 'deployer@47.107.71.191'])
 
 @task('deploy', ['on' => ['web']])
+su root -c 'chown -R deployer /var/www/deployment'
 cd /var/www/LaravelShop
 git pull origin master
 composer install
