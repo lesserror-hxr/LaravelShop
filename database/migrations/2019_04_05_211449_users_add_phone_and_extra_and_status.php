9d46680a86f2928a6490421871bdca9aa6b79744
Flourishing <huangxiangrong827@163.com>
2019-04-05 21:17:56 +0800

    add users migration files

---
+++ database/migrations/2019_04_05_211449_users_add_phone_and_extra_and_status.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UsersAddPhoneAndExtraAndStatus extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('phone')->nullable();
+            $table->text('extra')->nullable();
+            $table->char('status')->default(1)->comment('1为正常用户，0为已禁用');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('phone');
+            $table->dropColumn('extra');
+            $table->dropColumn('status');
+        });
+    }
+}
