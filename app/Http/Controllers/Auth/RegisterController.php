55ec44851de04b8394983fde3a1cfe3646b645d1
Flourishing <huangxiangrong827@163.com>
2019-04-07 01:00:23 +0800

    modified user login logic

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -73,7 +73,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        dd("123");
+        dd($data);
         return User::create([
             'name'     => $data['name'],
             'phone'    => $data['phone'],
